3350950bdee003a0dff425a5c787169d772deda6
Eduardo Lezcano Alvarez <contact@eduardolezcano.com>
2017-01-20 22:22:12 +0100

    Added server recipe and attributes

---
+++ attributes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook:: devpio
+# Attribute Set:: default
+#
+# Copyright:: 2017, Eduardo Lezcano
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+default['devpi']['server']['url']      = 'http://localhost'
+default['devpi']['server']['port']     = 3141
+
+default['devpi']['server']['home_dir'] = '/home/devpi'
+default['devpi']['server']['data_dir'] = '/var/devpi'
+default['devpi']['server']['user']     = 'devpi'
+default['devpi']['server']['group']    = 'devpi'
+
+default['devpi']['server']['name']     = 'devpi-server'
+default['devpi']['server']['version']  = nil
---
+++ recipes/server.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook:: devpio
+# Recipe:: server
+#
+# Copyright:: 2017, Eduardo Lezcano
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_recipe 'poise-python'
+
+server_home    = node['devpi']['server']['home_dir']
+server_data    = node['devpi']['server']['data_dir']
+server_user    = node['devpi']['server']['user']
+server_group   = node['devpi']['server']['group']
+
+server_name    = node['devpi']['server']['name']
+server_version = node['devpi']['server']['version']
+
+user server_user do
+  gid server_group
+  home server_home
+  shell '/bin/false'
+  system true
+end
+
+group server_group do
+  system true
+end
+
+directory server_home do
+  owner server_user
+  group server_group
+end
+
+directory server_data do
+  owner server_user
+  group server_group
+  mode 0o770
+  recursive true
+end
+
+python_package server_name do
+  version server_version unless server_version.nil?
+end
