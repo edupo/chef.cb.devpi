242b94e6ef32a9208a10be4625ccdd2d51586b6f
Eduardo Lezcano Alvarez <contact@eduardolezcano.com>
2017-01-21 15:29:01 +0100

    Added some basic inspec tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,57 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+raise if os.windows?
+
+# System
+describe user('root') do
+  it { should exist }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe user('devpi') do
+  it { should exist }
+end
+
+describe group('devpi') do
+  it { should exist }
+end
+
+# Files and folders
+
+describe directory('/home/devpi') do
+  it { should be_owned_by 'devpi' }
+  it { should be_grouped_into 'devpi' }
+end
+
+describe directory('/var/devpi') do
+  it { should be_owned_by 'devpi' }
+  it { should be_grouped_into 'devpi' }
+end
+
+describe directory('/home/devpi/bin') do
+  it { should be_readable.by_user('devpi') }
+  it { should be_executable.by_user('devpi') }
+end
+
+describe file('/home/devpi/bin/devpi-server') do
+  it { should be_readable.by_user('devpi') }
+  it { should be_executable.by_user('devpi') }
+end
+
+describe file('/var/devpi/.nodeinfo') do
+  it { should be_owned_by 'devpi' }
+  it { should be_grouped_into 'devpi' }
+end
+
+# Processes
+
+describe processes('devpi-server') do
+  its('list.length') { should eq 1 }
+  its('users') { should eq ['devpi'] }
+end
+
+# Network
+
+describe port(3141) do
+  it { should be_listening }
 end
