5fd2b9f411a85f2c9654c6972ddd502ee8b05631
Eduardo Lezcano Alvarez <contact@eduardolezcano.com>
2017-01-21 10:55:43 +0100

    Added service creation to recipe

--- recipes/server.rb
+++ recipes/server.rb
@@ -18,39 +18,82 @@
 
 include_recipe 'poise-python'
 
-server_home    = node['devpi']['server']['home_dir']
-server_data    = node['devpi']['server']['data_dir']
-server_user    = node['devpi']['server']['user']
-server_group   = node['devpi']['server']['group']
-
-server_name    = node['devpi']['server']['name']
-server_version = node['devpi']['server']['version']
-
-group server_group do
+group node['devpi']['server']['group'] do
   system true
 end
 
-user server_user do
-  gid server_group
-  home server_home
+user node['devpi']['server']['user'] do
+  gid node['devpi']['server']['group']
+  home node['devpi']['server']['home_dir']
   shell '/bin/false'
   system true
 end
 
-directory server_home do
-  owner server_user
-  group server_group
+directory node['devpi']['server']['home_dir'] do
+  owner node['devpi']['server']['user']
+  group node['devpi']['server']['group']
 end
 
-directory server_data do
-  owner server_user
-  group server_group
-  mode 0o770
+directory node['devpi']['server']['data_dir'] do
+  owner node['devpi']['server']['user']
+  group node['devpi']['server']['group']
+  mode '0770'
   recursive true
 end
 
-python_virtualenv server_home
+python_virtualenv node['devpi']['server']['home_dir']
 
-python_package server_name do
-  version server_version unless server_version.nil?
+python_package node['devpi']['server']['name'] do
+  version node['devpi']['server']['version'] unless \
+    node['devpi']['server']['version'].nil?
+end
+
+if node['init_package'] == 'systemd'
+
+  execute 'systemctl-daemon-reload' do
+    command '/bin/systemctl --system daemon-reload'
+    action :nothing
+  end
+
+  template '/etc/systemd/system/devpi.service' do
+    source 'devpi.service.erb'
+    owner 'root'
+    group 'root'
+    mode '0775'
+    action :create
+    notifies :run, 'execute[systemctl-daemon-reload]', :immediately
+    notifies :restart, 'service[devpi]', :delayed
+    variables(
+      :name => node['devpi']['server']['name'],
+      :user => node['devpi']['server']['user'],
+      :group => node['devpi']['server']['group'],
+      :home_dir => node['devpi']['server']['home_dir'],
+      :data_dir => node['devpi']['server']['data_dir'],
+      :url => node['devpi']['server']['url'],
+      :port => node['devpi']['server']['port'],
+    )
+  end
+
+else
+
+  template 'etc/init.d/devpi' do
+    source 'devpi.init.erb'
+    mode '0775'
+    notifies :restart, 'service[devpi]', :delayed
+    variables(
+      :name => node['devpi']['server']['name'],
+      :user => node['devpi']['server']['user'],
+      :group => node['devpi']['server']['group'],
+      :home_dir => node['devpi']['server']['home_dir'],
+      :data_dir => node['devpi']['server']['data_dir'],
+      :url => node['devpi']['server']['url'],
+      :port => node['devpi']['server']['port'],
+    )
+  end
+
+end
+
+service 'devpi' do
+  supports :status => true, :restart => true, :start => true, :stop => true
+  action :enable
 end
