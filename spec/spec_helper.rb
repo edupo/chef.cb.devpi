184e1b9a18baa65c1195f9c21691560034851d59
Eduardo Lezcano Alvarez <contact@eduardolezcano.com>
2017-01-22 22:37:38 +0100

    Added Junit formatter to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
   config.color = true
+  config.add_formatter('RspecJunitFormatter', 'rspec.xml')
 end
 
 at_exit { ChefSpec::Coverage.report! }
