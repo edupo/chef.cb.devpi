6ef0cf753ca5bc1b86c3817fd2c9c11b3da93beb
Eduardo Lezcano Alvarez <contact@eduardolezcano.com>
2017-02-12 18:26:00 +0100

    Moved user to a LWRP resource following best practices

---
+++ resources/user.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook:: devpio
+# Resource:: user
+#
+# Copyright:: 2017, Eduardo Lezcano
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+actions :create, :remove
+default_action :create
+
+# :password of the user
+# :server_url where the user exists
+
+property :password, String
+property :server_url, String,
+         default: "http://localhost:#{node['devpi']['server']['port']}"
+
+extend DevpiClientHelpers
+
+action_class do
+  # Mixin helpers
+  include DevpiClientHelpers
+
+  # If not defined by default
+  use_inline_resources
+
+  # Whyrun supported
+  def whyrun_supported?
+    true
+  end
+end
+
+action :create do
+  if user_exists?
+    Chef::Log.info("#{new_resource} exists - skipping")
+  else
+    converge_by("Create #{new_resource}") do
+      create
+    end
+  end
+end
+
+action :remove do
+  if user_exists?
+    Chef::Log.info("#{new_resource} does not exist - skipping")
+  else
+    converge_by("Remove #{new_resource}") do
+      remove
+    end
+  end
+end
